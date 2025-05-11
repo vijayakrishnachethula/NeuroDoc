da9cf916c122c821250d1ac5cb94b7dc25f85109
vijayakrishnachethula <125571022+vijayakrishnachethula@users.noreply.github.com>
2025-05-11 17:22:58 +0530

    Add files via upload

---
+++ c_vijaya_krishna/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+import os
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='Converso',
+    version='1.0.0',
+    packages=[''],
+    install_requires=[required],
+    url='',
+    license='',
+    author='Hrushikesh',
+    author_email='',
+    description=''
+)
